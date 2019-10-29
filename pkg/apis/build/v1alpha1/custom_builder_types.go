10f0a92c1a72860b038505c23cdaf84223d92b9d
Cole Abramson <cabramson@pivotal.io>
2019-10-29 17:13:14 -0400

    WIP: Created the types
    - Added buildpackage file used to create
    the build package
    
    Signed-off-by: Joao Pereira <jdealmeidapereira@pivotal.io>

---
+++ pkg/apis/build/v1alpha1/custom_builder_types.go
@@ -0,0 +1,56 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+const CustomBuilderKind = "CustomBuilder"
+
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object,k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMetaAccessor
+
+type CustomBuilder struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CustomBuilderSpec `json:"spec"`
+	Status BuilderStatus     `json:"status"`
+}
+
+type CustomBuilderSpec struct {
+	Tag   string  `json:"tag"`
+	Stack Stack   `json:"stack"`
+	Store Store   `json:"store"`
+	Order []Group `json:"order"`
+}
+
+type Stack struct {
+	BaseBuilderImage string `json:"baseBuilderImage"`
+}
+
+type Store struct {
+	Image string `json:"image"`
+}
+
+type Group struct {
+	Group []Buildpack `json:"group"`
+}
+
+type Buildpack struct {
+	ID string `json:"id"`
+}
+
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type CustomBuilderList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []CustomBuilder `json:"items"`
+}
+
+func (*CustomBuilder) GetGroupVersionKind() schema.GroupVersionKind {
+	return SchemeGroupVersion.WithKind(ClusterBuilderKind)
+}
